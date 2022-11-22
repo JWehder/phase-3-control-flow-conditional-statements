0a202693a015aa82efb1738e385dd9a1cc61b0c8
Jake Wehder <jw@email.comjake>
2022-11-22 14:09:48 -0600

    commit tree 910a1fc69a832e66efcf38f72b592d11e4a2444c

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,54 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    return "Access granted"
+  else 
+    return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  weather_statement = 
+  if temperature <= 40
+    "It's brisk out there!"
+  elsif (temperature >= 40 && temperature <= 65)
+    "It's a little chilly out there!"
+  elsif temperature >= 85
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
+  weather_statement
 end
 
 def fizzbuzz(num)
   # your code here
+  fizzbuzz = 
+  if (num % 3 == 0 && num % 5 == 0)
+    "FizzBuzz"
+  elsif (num % 3 == 0)
+    "Fizz"
+  elsif (num % 5 == 0)
+    "Buzz"
+  else 
+    num
+  end
+  fizzbuzz
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  solution = 
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "/" then num1 / num2
+  when "*" then num1 * num2
+  else
+    puts "Invalid operation!"
+    nil
+  end
+  solution
 end
 
